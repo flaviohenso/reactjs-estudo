12ae99a672bd4cb7fcf4eabcae8d6aef18e5598f
Flavio Henrique S Cabral <flavio.henso@gmail.com>
2020-05-16 15:55:24 -0300

    ReactJS

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -5,22 +5,55 @@ import './styles.css'
 
 export default class Main extends React.Component {
 
+    /* O state do componente é similar as props, mas é privado e totalmente controlado pelo componente. */
     state = {
         products: [],
+        productInfo: {},
+        page: 1,
     }
 
+    /* Metodos do ciclo de vida */
+    /* sempre é chamado pelo React após a saida do componete ter sido renderizada no DOM */
     componentDidMount() {
         this.loadProdutcs()
     }
 
-    loadProdutcs = async () => {
-        const response = await api.get('/products')
-        this.setState({ products: response.data.docs })
+    componentWillUnmount() {
+
+    }
+    /* Fim dos metodos do ciclo de vida */
+
+    loadProdutcs = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`)
+
+        const { docs, ...productInfo } = response.data
+
+        this.setState({ products: docs, productInfo, page})
+    }
+
+    prevPage = () => {
+        const { page, productInfo } = this.state
+
+        if (page <= 1) return null
+
+        const pageNumber = page - 1
+
+        this.loadProdutcs(pageNumber)
+    }
+
+    nextPage = () => {
+        const { page, productInfo } = this.state
+
+        if (page === productInfo.pages) return null
+
+        const pageNumber = page + 1
+
+        this.loadProdutcs(pageNumber)
     }
 
     render() {
 
-        const { products } = this.state
+        const { products,page,productInfo } = this.state
 
         return (
             <div className="product-list">
@@ -33,8 +66,8 @@ export default class Main extends React.Component {
                     </article>
                 ))}
                 <div className="actions">
-                    <button>Anterior</button>
-                    <button>Proximo</button>
+                    <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+                    <button disabled={page === productInfo.pages} onClick={this.nextPage}>Proximo</button>
                 </div>
             </div>
         )
